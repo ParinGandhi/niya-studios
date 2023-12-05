83285f03f2a81efd2debcef52f0a51f55c83e569
Parin Gandhi <parin.a.gandhi@gmail.com>
2023-12-04 23:23:44 -0500

    Added bootstrap and masonary view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NgxMasonryModule } from 'ngx-masonry';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), NgxMasonryModule, BrowserAnimationsModule],
+  exports: [RouterModule, NgxMasonryModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgxMasonryOptions } from 'ngx-masonry';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  ngOnInit(): void {
+    this.dialogElement = document.querySelector("dialog");
+  }
+
+  dialogElement: any = null;
   title = 'niya-studios';
+  selectedImage: string = '';
+
+  public myOptions: NgxMasonryOptions = {
+    gutter: 10
+  };
+
+  masonryItems = [
+    { imgUrl: '../assets/chicago01.jpg' },
+    { imgUrl: '../assets/chicago02.jpg' },
+    { imgUrl: '../assets/chicago03.jpg' },
+    { imgUrl: '../assets/chicago04.jpg' },
+    { imgUrl: '../assets/chicago05.jpg' },
+    { imgUrl: '../assets/chicago06.jpg' },
+    { imgUrl: '../assets/chicago07.jpg' },
+    { imgUrl: '../assets/chicago08.jpg' },
+    { imgUrl: '../assets/chicago09.jpg' },
+    { imgUrl: '../assets/chicago10.jpg' },
+    { imgUrl: '../assets/chicago11.jpg' },
+    { imgUrl: '../assets/chicago12.jpg' },
+    { imgUrl: '../assets/chicago13.jpg' },
+    { imgUrl: '../assets/chicago14.jpg' },
+    { imgUrl: '../assets/chicago15.jpg' },
+  ];
+
+  setSelectedImage(selectedImage: string) {
+    this.selectedImage = selectedImage;
+    this.dialogElement.showModal();
+  }
+
+  closeModal() {
+    this.dialogElement.close();
+  }
 }
